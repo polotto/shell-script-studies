d343c1b0252fd723b597e010be82f13781bfd6a6
Ângelo Polotto <angelopolotto@yahoo.com.br>
2019-03-28 09:43:02 -0300

    algumas atualizações no script

--- Scripts/OpenPortTest.sh
+++ Scripts/OpenPortTest.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+URL="http://portquiz.net"
+CURL_STD_OUT="/dev/null"
+CURL_ERR_OUT="/dev/null"
 LOG="./OpenPorts.txt"
 TIMEOUT=1
+# Port range to any proposal TCP use: 5001-49151
+# Source: http://cubicspot.blogspot.com/2016/04/need-random-tcp-port-number-for-your.html
+# Random gen: https://www.random.org/integers/?num=1&min=5001&max=49151&col=5&base=10&format=html&rnd=new
 MIN_PORT=5001
 MAX_PORT=49151
 
@@ -10,19 +16,20 @@ exec 2>&1
 
 echo
 echo
-echo "$(date) - Iniciando o teste de portas"
+echo "$(date) - Starting open port test (ref url: $URL)..."
 
 for PORT in $(seq $MIN_PORT $MAX_PORT)
 do
-	echo -n "-> Testando porta $PORT"
-	curl --max-time "$TIMEOUT" "http://portquiz.net:$PORT" > /dev/null 2>&1
+	echo -n "-> Testing port: $PORT"
+	curl --max-time "$TIMEOUT" "$URL:$PORT" >$CURL_STD_OUT 2>$CURL_ERR_OUT
 
 	if [ $? -eq 0 ]
 	then
-		echo " --> A porta $PORT está disponível"
+		echo " --> the port $PORT is open! (exit code: $?)"
 	else
-		echo " --> A porta $PORT NÃO está disponível"
+		echo " --> the port $PORT isnt open :-( (exit code: $?)"
 	fi
 done
 
+echo "$(date) - Open port testing finished."
 echo
